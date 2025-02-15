03ec3a7d545cf6592a8eb195ef85ae45c76afea8
Daniel Dexter Sanchez <danieldextersanchez@gmail.com>
2025-02-15 13:33:33 +0800

    init

---
+++ app/Projects/page.tsx
@@ -0,0 +1,10 @@
+import Header from '../components/header'
+import Content from '../components/content'
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Content title="test" body="body"/>
+    </main>
+  );
+}
---
+++ app/components/header/index.tsx
@@ -0,0 +1,10 @@
+import "../../globals.css";
+
+export default function Header(){
+    return <header className="bg-white">
+    <nav className="mx-auto flex max-w-7xl items-center justify-end p-8 space-x-4" aria-label="Global">
+      <a href="/" className="text-lg font-semibold">Home</a>
+      <a href="/Projects" className="text-lg font-semibold">Projects</a>
+    </nav>
+  </header>
+}

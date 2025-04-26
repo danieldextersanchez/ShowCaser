a0390f3b0c4db1f7c1c9665893be9f67e7d5a5d3
Daniel Dexter Sanchez <danieldextersanchez@gmail.com>
2025-04-27 03:27:21 +0800

    Minor enhancements + downgraded to  react 18 because Next.js does not support 19 yet

--- app/components/content-card/index.tsx
+++ app/components/content-card/index.tsx
@@ -3,13 +3,14 @@ import "../../globals.css";
 interface content {
     title : string,
     body : string,
-    image : string
+    image : string,
+    link ?: string
 }
 export default function Content(props : content){
     return <div className="p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
-        <h1>{props.title}</h1>
+        <h1>{props.link ? <a className="text-blue-500 underline" target="_blank" href={props.link}>{props.title} </a> : props.title}</h1>
         <div>
-            <div className="w-1/2 h-1/2"><img src="/cool.jpg" /></div>
+            <div className="w-1/2 h-1/2 ml-auto mr-auto"><img src={"/" + props.image}/></div>
             {props.body}
         </div>
     </div>
